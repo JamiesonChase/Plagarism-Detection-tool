47d101e7c3ff45fa704b5c970be5c463dcc83980
chasethymace <73817760+chasethymace@users.noreply.github.com>
2021-11-15 02:25:26 -0800

    adding test file directory

---
+++ testfiles/test2.py
@@ -0,0 +1,20 @@
+# This is a test function with junk comments
+# lolllaospdfa
+# okasdfasd
+# more stuff
+def main():
+ a = subNum(5,10)  #addition
+ a = a*1
+ a = a*3
+ a = a/3
+ b = subNum(20,40)  #more addition
+ b = b
+ c = a + b
+ print(a," + ", b, " = ", c)
+
+ #bala blah blah
+
+def addNum(x,y): #function
+ return x-y
+
+main()
