332dc2d836cda04dc45cea33b625d1703c002e8f
ptrevordacty1 <trevor.holland@wsu.edu>
2022-02-10 19:01:58 -0800

    Added translation of lines to correspond to the original files, rather than the processed ones

--- WorkFolder/comparisonAndHighlighting/highlightLines.py
+++ WorkFolder/comparisonAndHighlighting/highlightLines.py
@@ -10,7 +10,7 @@ def findMatches(string1, string2):
     -b = the start index of the match in string2
     -size = the length of the matching string
     """
-    MIN_MATCH_SIZE = 3 # A match must be at least this long 
+    MIN_MATCH_SIZE = 5 # A match must be at least this long 
     matches = []
 
     # return empty list if either string is empty
@@ -46,21 +46,14 @@ def getLines(string, matchMin, matchMax):
     maxLine = len(string[:matchMax+1].splitlines(keepends=True)) - 1
     return(minLine, maxLine)
     
-def getHighlightedLines(file1, file2):
+def matchProcessedLines(file1String, file2String):
     """
-    Finds matching sections of text between both file inputs, returning their corresponding line locations.
+    Finds matching sections of text between both string inputs, returning their corresponding line locations.
+    String inputs should be from preprocessed files
 
     Returns a tuple of the format:
     (file1LineBlocks=[(minLine0, maxLine0),(minLine1, maxLine1), ...], file2LineBlocks=[(minLine0, maxLine0),(minLine1, maxLine1), ...])
     """
-    file1String = ""
-    file2String = ""
-
-    # store each file in a string
-    with open(file1, mode = "r") as f1:
-        file1String = f1.read()
-    with open(file2, mode = "r") as f2:
-        file2String = f2.read()
 
     # get the matching sections of each file
     matches = findMatches(file1String, file2String)
@@ -70,6 +63,48 @@ def getHighlightedLines(file1, file2):
     file2LineBlocks = []
     for match in matches:
         file1LineBlocks.append(getLines(file1String, match.a, match.a + match.size))
-        file2LineBlocks.append(getLines(file2String, match.b, match.b + match.size))      
+        file2LineBlocks.append(getLines(file2String, match.b, match.b + match.size))
 
     return (file1LineBlocks, file2LineBlocks)
+
+def TranslateLines(StrippedFile,OldLines,SourceFile):
+    file = open(StrippedFile)
+    content = file.readlines()  # Get all lines from stripped file
+    newlines = []
+    i = 0
+    lookup = content[OldLines[0]-1].strip()  # initialize first line
+    with open(SourceFile) as myFile:  # iterate through source file
+        for num, line in enumerate(myFile, 1):
+            if lookup in line:  # if processed line is in source line
+                newlines.append(num)  # append source line value
+                if i < len(OldLines)-1:
+                    i = i+1  # iterate through each suspect old line
+                lookup = content[OldLines[i]-1].strip() #
+    return newlines  # return list of translated lines
+
+def getHighlightLines(file1, file2, processedFile1, processedFile2, strippedFile1, strippedFile2):
+
+    lines = matchProcessedLines(processedFile1, processedFile2)
+    file1Lines = lines[0]
+    file2Lines = lines[1]
+
+    newLines1 = []
+    newLines2 = []
+
+    # convert the line blocks from matching to line up with the original files
+    for lineBlock in file1Lines:
+        lines = list(range(lineBlock[0], lineBlock[1] + 1))
+        newLineBlock = TranslateLines(strippedFile1, lines, file1)
+        newLineBlock = (min(newLineBlock), max(newLineBlock)) 
+        newLines1.append(newLineBlock)
+
+    for lineBlock in file2Lines:
+        lines = list(range(lineBlock[0], lineBlock[1] + 1))
+        newLineBlock = TranslateLines(strippedFile2, lines, file2)
+        newLineBlock = (min(newLineBlock), max(newLineBlock)) 
+        newLines2.append(newLineBlock)
+
+    newLines1.sort(key=lambda a : a[0])
+    newLines2.sort(key=lambda a : a[0])
+
+    return (newLines1, newLines2)
