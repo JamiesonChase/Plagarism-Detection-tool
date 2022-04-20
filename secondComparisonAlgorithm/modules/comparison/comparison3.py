c726644e470d21d8d479dad242e02e04cdaac36f
ptrevordacty1 <trevor.holland@wsu.edu>
2022-04-19 19:08:33 -0700

    Added more documentation, made some small tweaks for readability

--- secondComparisonAlgorithm/modules/comparison/comparison3.py
+++ secondComparisonAlgorithm/modules/comparison/comparison3.py
@@ -36,10 +36,6 @@ def file_setup(document):
     s = invertedIndexCreate(s)
     return s
 
-def compareLine(hashes1, hashes2):
-    matches = set.intersection(set(hashes1), set(hashes2))
-    return min(len(matches) / len(hashes1), len(matches) / len(hashes2))
-
 def invertDict(matchedIndex):
     """
     Inverts the input dictionary 'matchedIndex' {ngram : [line numbers]}
@@ -48,9 +44,6 @@ def invertDict(matchedIndex):
     Complexity: O(m*l)
         - m is the number of matching ngrams
         - l is the average number of lines mapped to each ngram
-
-    TODO: make this more efficient with something like this:
-        my_inverted_dict = dict(map(reversed, my_dict.items()))
     """
     newIndex = {}
     for match in matchedIndex:
@@ -65,48 +58,98 @@ def invertDict(matchedIndex):
     return newIndex
 
 def compareLine(hashes1, hashes2):
+    """
+    Computes the ratio of the number of shared hashes in hashes1 and hashes2
+    to the total number of hashes in the larger of the two lists.
+
+    Compexity: O(min(len(s), len(t))) on average, O(len(s) * len(t)) worst case
+        - s is hashes1
+        - t is hashes2
+    """
     matches = set.intersection(set(hashes1), set(hashes2))
-    return min(len(matches) / len(hashes1), len(matches) / len(hashes2))
+    return len(matches) / max(len(hashes1), len(hashes2))
 
 def findLongestSequence(s1, s2, index1, index2):
+    """
+    Returns the length of the longest sequence of similar lines in index1 and index2
+    starting on lines s1 and s2 respectively.
+
+    TODO: time complexity
+    """
+
+    # base cases for recursion
     if s1 > len(index1) or s2 > len(index2):
+        # don't go past index bounds
         return 0
     if compareLine(index1[s1], index2[s2]) < MIN_HASH_THRESHOLD:
+        # once the two lines aren't similar enough, return
         return 0
     
+    # add 1
     return 1 + findLongestSequence(s1 + 1, s2 + 1, index1, index2)
 
-def getLineScore(s1, index1, index2):
+def getLineScore(line1, index1, index2):
+    """
+    Compares line1 from index1 to every line in index2, finding the longest sequence
+    at each one. Returns the maximum of longest sequences found, along with the line
+    number(s) that longest sequence was found at.
+
+    TODO: time complexity
+    """
     score = 0
     maxScore = 0
     bestLines = []
     
-    s2 = 1
-    while s2 < len(index2):
-        score = findLongestSequence(s1, s2, index1, index2)
+    # iterate over the lines in index2
+    line2 = 1
+    while line2 < len(index2):
+        # calculate the score for line1 on line2
+        score = findLongestSequence(line1, line2, index1, index2)
         if score > maxScore:
             maxScore = score
-            bestLines = [s2]
+            bestLines = [line2]
+        # when score is the same as max, add line2 to list of best lines
         elif score == maxScore and score > 0:
-            bestLines.append(s2)
-        s2 += max(score, 1)
+            bestLines.append(line2)
+        # increment line2
+        line2 += max(score, 1)
         
     return [maxScore, bestLines]
 
 def adjustScores(startLine, scores):
-    if scores[startLine][0] < 2 or startLine + 1 > len(scores):
+    """
+    Adjusts any improper line scores that appear from overlapping blocks.
+    For example, if a sequence of lines in the scores dictionary looks like this:
+    [5, 4, 3, 2, 5, ...] it will be adjusted to this: [4, 3, 2, 1, 5, ...].
+    This is to help with constructing blocks later.
+
+    TODO: time complexity
+    """
+    # base cases for recursion
+    if scores[startLine][0] < 2 or startLine >= len(scores):
+        # score is too small or startLine is out of bounds
         return 0
     if scores[startLine + 1][0] >= scores[startLine][0]:
+        # the next score is greater than the current one, decrement the current score
         return scores[startLine][0] - 1
+    # go to next line
     return adjustScores(startLine + 1, scores)
 
 def getBlocks(scores):
+    """
+    Returns a list of blocks constructed from the scores dictionary.
+    For example, if line 3 in scores has a score of 4, then the longest sequence
+    that line 3 matched with was 4 lines long, so the block constructed is (3, 6).
+
+    TODO: time complexity
+    """
     blocks = []
     block = 1
     while block < len(scores):
         score = scores[block][0]
+        # don't include blocks that are only 1 line long
+        # these blocks tend to match to ~many~ lines in the opposite file and are a pain
         if score > 1:
-            # don't forget about the tuple
             blocks.append((block, block + score - 1))
             block += score
         else:
@@ -114,6 +157,12 @@ def getBlocks(scores):
     return blocks
 
 def translateLines(SourceFile, StrippedFile):
+    """
+    Returns a list of lines from SourceFile that the lines in StrippedFile
+    were translated from.
+
+    TODO: time complexity
+    """
     file = open(StrippedFile)
     content = file.readlines()  # Get all lines from stripped file
     newlines = []
@@ -130,48 +179,84 @@ def translateLines(SourceFile, StrippedFile):
                     lookup = content[i].strip() #
     return newlines  # return list of translated lines
 
-def isInBlock(line, block):
-    return line >= block[0] and line <= block[1]
-
 def getBlockFromLine(line, blocks):
+    """
+    Returns the block from 'blocks' that the given line number belongs to,
+    so long as that block exists.
+
+    TODO: time complexity
+    """
     for block in blocks: 
-        if isInBlock(line, block):
+        # line is within the blocks start and end lines.
+        if line >= block[0] and line <= block[1]:
             return block
 
 def getBlockMappings(scores, oppBlocks):
+    """
+    Returns a dictionary of blocks from one file mapped to a
+    set of corresponding blocks in the opposite file.
+    
+    TODO: time complexity
+    """
     mappings = {}
     i = 1
     while i < len(scores):
         score = scores[i][0]
-        if score <= 1:
+        # ignore blocks less than length 2
+        if score < 2:
             i += 1
             continue
+        # get all blocks that the current line number maps to
         lineMaps = scores[i][1]
         blockMaps = set()
         for line in lineMaps:
+            # set addition ignores duplicates
             blockMaps.add(getBlockFromLine(line, oppBlocks))
+        # add this mapping to the dictionary and increment the line number
         mappings[(i, i + score - 1)] = list(blockMaps)
         i += score
     return mappings
 
-def getFullPath(block, oppBm):
+def getFullPath(block, oppBlockMappings):
+    """
+    Builds a set, 'blocksThere', of all the blocks in the opposite file that map back to 'block'.
+    Builds another set, 'blocksHere', of the all the blocks in the same file that are also mapped back to
+    by blocks in the opposite file.
+    This feels like an inelegant way to do this, but I was unable to come up with anything better.
+    I apologize if this method is confusing.
+
+    TODO: time complexity
+    """
     blocksHere = set()
     blocksThere = set()
-    q = [block]
+    queue = [block]
     blocksHere.add(block)
 
-    while(len(q) > 0):
-        b = q.pop()
-        for k in oppBm:
-            if b in oppBm[k]:
+    # loop until no new blocks are found
+    while(len(queue) > 0):
+        b = queue.pop()
+        # find any blocks from the opposite file that map back to the block 'b'
+        for k in oppBlockMappings:
+            if b in oppBlockMappings[k]:
+                # store (without duplicates) the block from opposite file
                 blocksThere.add(k)
-                for i in oppBm[k]:
+                for i in oppBlockMappings[k]:
+                    # if the block 'i' hasn't been seen yet, add it to the queue
                     if i not in blocksHere:
-                        q.append(i)
+                        queue.append(i)
+                    # store (without duplicates) any blocks that the blocks from the opposite file map back to
                     blocksHere.add(i)
+    # return both sets
     return (blocksHere, blocksThere)
 
 def highlightedBlocks(file1, file2):
+    """
+    Returns two lists of the format [[colorNum, (startLine, endLine)], ...]
+    - colorNum is a number to uniquely identify which color to assign the following block
+    - startLine is the beginning line of a block of code to highlight, endLine is the end of that same block
+    - the first list corresponds to file1, and the second to file2
+    """
+    # set up the indices
     index1 = file_setup(file1)
     index2 = file_setup(file2)
 
@@ -179,16 +264,19 @@ def highlightedBlocks(file1, file2):
     index1 = invertDict(index1)
     index2 = invertDict(index2)
 
-    
+    # build two dictionaries of the format {lineNumber: [score, [bestLines]]}
+    # where score represents the longest matching sequence that line corresponds to
+    # and bestLines is the list of lines that longest matching sequence begin on
     scores1 = {}
     for s1 in index1:
         scores1[s1] = getLineScore(s1, index1, index2)
-    for i in range(1, len(scores1)):
-        scores1[i][0] -= adjustScores(i, scores1)
-    
     scores2 = {}
     for s2 in index2:
         scores2[s2] = getLineScore(s2, index2, index1)
+
+    # adjust for overlaps (see adjustScores for more info)
+    for i in range(1, len(scores1)):
+        scores1[i][0] -= adjustScores(i, scores1)
     for i in range(1, len(scores2)):
         scores2[i][0] -= adjustScores(i, scores2)
 
@@ -201,6 +289,7 @@ def highlightedBlocks(file1, file2):
         for key, value in sorted(scores2.items()):
             print("{} : {}".format(key, value))
 
+    # build two lists of blocks from the respective scores dictionaries (see getBlocks for more info)
     blocks1 = getBlocks(scores1)
     blocks2 = getBlocks(scores2)
 
@@ -208,6 +297,7 @@ def highlightedBlocks(file1, file2):
         print("blocks1 = " + str(blocks1))
         print("blocks2 = " + str(blocks2))
 
+    # build two dictionaries of the format {block1: [blocks2]} (sett getBlockMappings for more info)
     blockMappings1 = getBlockMappings(scores1, blocks2)
     blockMappings2 = getBlockMappings(scores2, blocks1)
 
@@ -220,6 +310,8 @@ def highlightedBlocks(file1, file2):
     matchedBlocks2 = []
     i = 0
 
+    # for each block, find all similar blocks and assign them all the same number (for coloring).
+    # remove each of these blocks from their respective lists along the way until no blocks remain
     while len(blocks1) > 0:
         block = blocks1.pop()
         res = getFullPath(block, blockMappings2)
@@ -242,6 +334,7 @@ def highlightedBlocks(file1, file2):
             if b in blocks2: blocks2.remove(b)
         i += 1
 
+    # sort the blocks by order of start line (assuming no blocks should overlap, this is sufficient)
     matchedBlocks1.sort(key = lambda x: x[1][0])
     matchedBlocks2.sort(key = lambda x: x[1][0])
 
@@ -249,14 +342,15 @@ def highlightedBlocks(file1, file2):
         print("matchedBlocks1 = " + str(matchedBlocks1))
         print("matchedBlocks2 = " + str(matchedBlocks2))
 
+    # get the line translations (see translateLines for more info)
     file1Lines = translateLines(file1, file1 + "_Stripped")
     file2Lines = translateLines(file2, file2 + "_Stripped")
 
+    # finally, translate the lines of each block to line up with the original files
     for block in matchedBlocks1:
         block[1] = (file1Lines[block[1][0] - 1], file1Lines[block[1][1] - 1])
     for block in matchedBlocks2:
         block[1] = (file2Lines[block[1][0] - 1], file2Lines[block[1][1] - 1])
-        
 
     if DEBUG:
         print("matchedBlocks1 after translating = " + str(matchedBlocks1))
