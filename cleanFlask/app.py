d22990ba3c88131b4b382bcb8b44a0f62a2dc1d0
Vinh Duong <vinh.duong@wsu.edu>
2022-04-29 09:30:34 -0700

    Add command line interface to app.py

--- cleanFlask/app.py
+++ cleanFlask/app.py
@@ -10,12 +10,11 @@ import difflib as dl
 from modules.preprocessing.process import process
 from modules.hashingFingerprinting.hashFingerprint import hashingFunction
 from modules.winnowing.winnowing import winnow
-from modules.comparison.comparison import highlightedBlocks
-# from modules.comparison.comparison3 import highlightedBlocks
-# a,b = highlightedBlocks(file1,file2)
+from modules.comparison.comparison3 import highlightedBlocks
 import shutil 
 import time
 import pickle
+import sys 
 
 app = Flask(__name__)
 global items 
@@ -35,6 +34,119 @@ justInputInformation = []
 ALLOWED_EXTENSIONS = set(['py', 'c', 'java']) #Only these extensions will be allowed to be uploaded
 app.secret_key = "secret key" #Secret key to make the seure_filename upload work.
 
+def createNgrams(directory):
+    documents = os.listdir(directory)  # find documents inside testfiles directory
+    lists = []
+    for doc in documents: # Go through the process of getting the winnowed information for the inputs
+        originalName = doc 
+        doc = directory + doc
+        a=process(doc)
+        a=hashingFunction(a,7)
+        a=winnow(4,a)
+        lists.append([a,doc,originalName]) # winnow the hashes and append to lists
+
+    return lists 
+
+def compareAndPrint(lists):
+    idNumber = 0
+
+    r=[] # Just calculate similairty based off of the inputs
+    for i in range(0,len(lists)):
+        a = lists[i][0]
+        a = [lis[1] for lis in a] # do intersection comparison
+        for j in range(i+1,len(lists)):
+            b = lists[j][0]
+            b = [lis[1] for lis in b]
+            s = dl.SequenceMatcher(None, a, b) # sequence match a&b
+            sum = 0
+            lines = 0
+            for block in s.get_matching_blocks(): #get matching blocks
+                sum = sum + block[2] # calculate total matched hashes
+                if block[0] < len(a)-1 and block[1] < len(b)-1: # calculate lines matched
+                    lines = lines + -lists[i][0][block[0]][0] + lists[i][0][block[0]+block[2]-1][0] + 1
+            #items[idNumber] = lists[i][1] + ' - ' + corpusLists[j][1]
+            r.append([idNumber, lists[i][1] + ' - ' + lists[j][1],100*sum/min(len(a),len(b)),lines]) # append to result
+            idNUmber = idNumber + 1
+
+    r = sorted(r, key=lambda tup: tup[2], reverse=True) # sort by lines matched, tup[2] -> tup[1] to sort by %
+
+    total = 0
+    num = 0
+    for x in r:
+        print(x)
+        
+        total = total + x[2]
+        num = num + 1
+
+    average = total / num 
+    print("Average is: " + str(average))
+
+def comparisonsFilter(lists):
+    idNumber = 0
+
+    r=[] # Just calculate similairty based off of the inputs
+    for i in range(0,len(lists)):
+        a = lists[i][0]
+        a = [lis[1] for lis in a] # do intersection comparison
+        leftFileNameKey = lists[i][1].split("_")
+        leftFileNameKey = leftFileNameKey[0]
+        for j in range(i+1,len(lists)):
+            rightFileNameKey = lists[j][1].split("_")
+            rightFileNameKey = rightFileNameKey[0]
+            if (rightFileNameKey == leftFileNameKey):
+                continue
+
+            b = lists[j][0]
+            b = [lis[1] for lis in b]
+            s = dl.SequenceMatcher(None, a, b) # sequence match a&b
+            sum = 0
+            lines = 0
+            for block in s.get_matching_blocks(): #get matching blocks
+                sum = sum + block[2] # calculate total matched hashes
+                if block[0] < len(a)-1 and block[1] < len(b)-1: # calculate lines matched
+                    lines = lines + -lists[i][0][block[0]][0] + lists[i][0][block[0]+block[2]-1][0] + 1
+            #items[idNumber] = lists[i][1] + ' - ' + corpusLists[j][1]
+            r.append([idNumber, lists[i][1] + ' - ' + lists[j][1],100*sum/min(len(a),len(b)),lines]) # append to result
+            idNUmber = idNumber + 1
+
+    r = sorted(r, key=lambda tup: tup[2], reverse=True) # sort by lines matched, tup[2] -> tup[1] to sort by %
+
+    total = 0
+    num = 0
+    for x in r:
+        print(x)
+        
+        total = total + x[2]
+        num = num + 1
+
+    average = total / num 
+    print("Average is: " + str(average))
+
+def cleanDirectory(directory):
+    files_in_directory = os.listdir(directory)
+    filtered_files = [file for file in files_in_directory if file.endswith("_Stripped")]
+    for file in filtered_files:
+        path_to_file = os.path.join(directory, file)
+        os.remove(path_to_file)
+
+if (len(sys.argv) == 1): # Just running it regularly.
+    print("Continue onto regular flask application")
+elif (len(sys.argv) == 2):
+    result = createNgrams(sys.argv[1])
+    compareAndPrint(result)
+    cleanDirectory(sys.argv[1])
+    exit()
+elif (len(sys.argv) == 3):
+    if (sys.argv[1] != "-f"):
+        print("Format: python3 [input directory] or python3 -f [input directory]")
+        exit()
+    result = createNgrams(sys.argv[2])
+    comparisonsFilter(result)
+    cleanDirectory(sys.argv[2])
+    exit()
+else:
+    print("Format: python3 [input directory] or python3 -f [input directory]")
+    exit()
 
 class LinkDeciderCol(LinkCol): # will be deleted
 
@@ -128,6 +240,7 @@ def upload_corpus():
     global r 
     global idNumber
     global justCorpusInformation
+    global items 
 
     if request.method == 'POST': # If it is the POST request.
         if 'files[]' not in request.files: # If no files were selected display a message on the page.
@@ -203,6 +316,7 @@ def upload_corpus():
                     sum = sum + block[2] # calculate total matched hashes
                     if block[0] < len(a)-1 and block[1] < len(b)-1: # calculate lines matched
                         lines = lines + -lists[i][0][block[0]][0] + lists[i][0][block[0]+block[2]-1][0] + 1
+                #items[idNumber] = lists[i][1] + ' - ' + corpusLists[j][1]
                 r.append([idNumber, lists[i][1] + ' - ' + corpusLists[j][1],100*sum/min(len(a),len(b)),lines]) # append to result
                 idNUmber = idNumber + 1
 
@@ -217,6 +331,7 @@ def loadCorpusFile():
     global r 
     global idNumber
     global justCorpusInformation
+    global items 
 
     corpusName = request.args.get('fileName') #Load the specific corpus and assign it to the dictionary table.
     #pickleName = corpusName[:-5] + ".pkl"
@@ -256,6 +371,7 @@ def loadCorpusFile():
                 sum = sum + block[2] # calculate total matched hashes
                 if block[0] < len(a)-1 and block[1] < len(b)-1: # calculate lines matched
                     lines = lines + -lists[i][0][block[0]][0] + lists[i][0][block[0]+block[2]-1][0] + 1
+            #items[idNumber] = lists[i][1] + ' - ' + corpusLists[j][1]
             r.append([idNumber, lists[i][1] + ' - ' + corpusLists[j][1],100*sum/min(len(a),len(b)),lines]) # append to result
             idNUmber = idNumber + 1
 
@@ -268,16 +384,9 @@ def mainTable():
     global items
     global r 
 
-    i = 0
-    total = 0
-    for x in r:
-        total = total + x[2]
-        i = i + 1
-
-    print(total/i)
 
 
-    items = [(0,'testfiles/file1.py - testfiles/file2.py', 57.2, 20), (1,'testfiles/file1.py - testfiles/file2.py', 57.2, 20)]
+    #items = [(0,'testfiles/file1.py - testfiles/file2.py', 57.2, 20), (1,'testfiles/file1.py - testfiles/file2.py', 57.2, 20)]
     
     return render_template("mainTable.html",htmlTable = r) # Load the main compairison page.
 
@@ -286,6 +395,7 @@ def noCorpus():
     global r
     global idNumber 
     global lists 
+    global items
 
     r=[] # Just calculate similairty based off of the inputs
     for i in range(0,len(lists)):
@@ -301,6 +411,7 @@ def noCorpus():
                 sum = sum + block[2] # calculate total matched hashes
                 if block[0] < len(a)-1 and block[1] < len(b)-1: # calculate lines matched
                     lines = lines + -lists[i][0][block[0]][0] + lists[i][0][block[0]+block[2]-1][0] + 1
+            #items[idNumber] = lists[i][1] + ' - ' + corpusLists[j][1]
             r.append([idNumber, lists[i][1] + ' - ' + lists[j][1],100*sum/min(len(a),len(b)),lines]) # append to result
             idNUmber = idNumber + 1
 
